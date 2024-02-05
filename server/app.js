683559d595a0125fa448ed5fdd737ea91e692785
Rahul Reddy <rahulreddy.a@hotmail.com>
2024-02-05 10:32:30 +0530

    created server

--- server/app.js
+++ server/app.js
@@ -5,12 +5,7 @@ const PORT = require("./config").PORT
 const homeRoute = require('./routes/homeRoute')
 
 const app = express();
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/',homeRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
