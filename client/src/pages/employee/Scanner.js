0d7904554b3ce5313483f8a2260d344ca02f421c
KamalapurNaveen <kamalapurnaveen147@gmail.com>
2024-02-19 10:44:43 +0530

    fixed bugs in scanner

--- client/src/pages/employee/Scanner.js
+++ client/src/pages/employee/Scanner.js
@@ -1,12 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
 import QrReader from 'react-qr-scanner';
-import { Row, Col, Modal, Button, Tag, message ,Input} from 'antd';
-import API_LINK from '../../util/api.link'
+import { Row, Col, Modal, Button, Tag, message, Input } from 'antd';
+import API_LINK from '../../util/api.link';
 
 const Scanner = () => {
   const [result, setResult] = useState('');
   const [orderData, setOrderData] = useState(null);
-  const [otp,setOtp] =useState(null);
+  const [otp, setOtp] = useState(null);
   const [userData, setUserData] = useState(null);
   const [walletData, setWalletData] = useState(null);
   const [amountToAdd, setAmountToAdd] = useState('');
@@ -14,6 +14,7 @@ const Scanner = () => {
   const [showWalletModal, setShowWalletModal] = useState(false);
   const [permissionGranted, setPermissionGranted] = useState(false);
   const [scannerVisible, setScannerVisible] = useState(true); // Track visibility of scanner
+  const [selectedDeviceId, setSelectedDeviceId] = useState(null); // Track selected camera device
   const scannerRef = useRef(null);
 
   useEffect(() => {
@@ -22,15 +23,35 @@ const Scanner = () => {
 
   const requestCameraPermission = async () => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }); // Use rear camera by default
       setPermissionGranted(true);
       // Start scanning if permission is granted
       if (scannerRef.current) {
         scannerRef.current.streamRef.current.srcObject = stream;
-        scannerRef.current.startScan();
+        scannerRef.current.startScan(selectedDeviceId);
       }
     } catch (error) {
       console.error('Error accessing camera:', error);
+    }
+  };
+
+  const handleScan = (data) => {
+    if (data) {
+      console.log(data.text);
+
+      // Split the scanned data into an array using '|' as the delimiter
+      const [id, type] = data.text.split('|');
+      if (id && type) {
+        if (type === 'order') {
+          handleOrderScan(id);
+        } else if (type === 'wallet') {
+          handleWalletScan(id);
+        } else {
+          console.error('Unknown data type:', type);
+        }
+      } else {
+        console.error('Invalid scanned data format:', data.text);
+      }
     }
   };
 
@@ -40,11 +61,9 @@ const Scanner = () => {
       const resData = await response.json();
       setOrderData(resData.data["order"]);
       setShowOrderModal(true);
-      // Pause scanning when modal is displayed
       if (scannerRef.current) {
         scannerRef.current.pauseScan();
       }
-      // Hide the scanner
       setScannerVisible(false);
     } catch (error) {
       console.error('Error fetching order data:', error);
@@ -55,9 +74,7 @@ const Scanner = () => {
     try {
       const response = await fetch(`${API_LINK}/api/_e/profile/customer?id=${userId}`, { credentials: 'include' });
       const resData = await response.json();
-      console.log(resData);
       setUserData(resData.data);
-      console.log(userData);
       setShowWalletModal(true);
       if (scannerRef.current) {
         scannerRef.current.pauseScan();
@@ -68,14 +85,14 @@ const Scanner = () => {
     }
   };
 
-  const handleAddMoney = async (wallet_id,amountToAdd,otp) => {
+  const handleAddMoney = async (wallet_id, amountToAdd, otp) => {
     function isFourDigitNumber(str) {
       // Regular expression to match 4 digits
       const regex = /^\d{4}$/;
       return regex.test(str);
     }
-    if(!isFourDigitNumber(otp)) {
-      message.error('OTP should be 4 digit number');
+    if (!isFourDigitNumber(otp)) {
+      message.error('OTP should be a 4-digit number');
       return;
     }
 
@@ -101,45 +118,18 @@ const Scanner = () => {
   };
 
   const handleWalletScan = async (userId) => {
-      fetchUserData(userId);
+    fetchUserData(userId);
   };
 
-  const handleScan = (data) => {
-    if (data) {
-      //setShowOrderModal(true);
-      console.log(data.text);
-      
-      // Split the scanned data into an array using '|' as the delimiter
-      const [id, type] = data.text.split('|');
-      if (id && type) {
-        if (type === 'order') {
-        
-          handleOrderScan(id);
-        } else if (type === 'wallet') {
-          // Handle wallet scan
-    
-          handleWalletScan(id);
-        } else {
-          // If type is neither order nor wallet, log an error or handle it accordingly
-          console.error('Unknown data type:', type);
-        }
-      } else {
-        console.error('Invalid scanned data format:', data.text);
-      }
-    }
-  };
-  
   const handleModalClose = () => {
     setShowOrderModal(false);
     setShowWalletModal(false);
-    // Resume scanning when modal is closed
     if (scannerRef.current) {
-      scannerRef.current.resumeScan();
+      scannerRef.current.resumeScan(selectedDeviceId);
     }
-    // Show the scanner
     setScannerVisible(true);
   };
-  
+
   const handleCancelOrder = async (orderId) => {
     try {
       const response = await fetch(`${API_LINK}/api/_e/order`, {
@@ -148,17 +138,15 @@ const Scanner = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          "order":{
-             _id: orderId,
-             status:"cancelled"
+          "order": {
+            _id: orderId,
+            status: "cancelled"
           }
         }),
         credentials: 'include',
       });
       const resData = await response.json();
-      console.log(resData);
       if (resData.success) {
-        console.log('Order cancelled successfully');
         message.success("Order cancelled successfully")
       } else {
         message.error(resData.message);
@@ -169,7 +157,7 @@ const Scanner = () => {
     }
     handleModalClose();
   };
-  
+
   const handleDeliverOrder = async (orderId) => {
     try {
       const response = await fetch(`${API_LINK}/api/_e/order`, {
@@ -178,17 +166,15 @@ const Scanner = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          "order":{
-             _id: orderId,
-             status:"delivered"
+          "order": {
+            _id: orderId,
+            status: "delivered"
           }
         }),
         credentials: 'include',
       });
       const resData = await response.json();
-      console.log(resData);
       if (resData.success) {
-        console.log('Order delivered successfully');
         message.success("Order delivered successfully")
       } else {
         message.error(resData.message);
@@ -199,18 +185,35 @@ const Scanner = () => {
     }
     handleModalClose();
   };
+
+  const handleDeviceChange = (deviceId) => {
+    setSelectedDeviceId(deviceId);
+    if (scannerRef.current) {
+      scannerRef.current.stopScan();
+      scannerRef.current.startScan(deviceId);
+    }
+  };
+
   return (
     <>
       <Row justify="center" align="middle" className="mt-4" style={{ height: 'calc(100vh - 64px)' }}>
         <Col xs={24} md={12} lg={8}>
-          {permissionGranted && scannerVisible && ( // Only render scanner if it's visible
-            <QrReader
-              ref={scannerRef}
-              delay={300}
-      
-              onScan={handleScan}
-              style={{ width: '100%', maxHeight: '70vh' }} // Adjust size of the scanner
-            />
+          {permissionGranted && scannerVisible && (
+            <>
+              <QrReader
+                ref={scannerRef}
+                delay={300}
+                onError={console.error}
+                onScan={handleScan}
+                style={{ width: '100%', maxHeight: '70vh' }}
+              />
+              <Row justify="center" style={{ marginBottom: '10px' }}>
+                <Col>
+                  <Button onClick={() => handleDeviceChange(null)}>Switch Camera</Button>
+                </Col>
+              </Row>
+
+            </>
           )}
         </Col>
       </Row>
@@ -219,7 +222,7 @@ const Scanner = () => {
         title="Order Details"
         visible={showOrderModal}
         onCancel={handleModalClose}
-        footer={orderData && (orderData.status === 'delivered' || orderData.status === 'cancelled' )? [
+        footer={orderData && (orderData.status === 'delivered' || orderData.status === 'cancelled') ? [
           <Button key="close" onClick={handleModalClose}>
             Close
           </Button>
@@ -255,7 +258,7 @@ const Scanner = () => {
             <ul>
               {orderData.items.map((item, index) => (
                 <li key={index}>
-                  {item.name} -  {item.quantity}
+                  {item.name} - {item.quantity}
                 </li>
               ))}
             </ul>
@@ -265,44 +268,44 @@ const Scanner = () => {
 
       {/* Add modal for wallet data if needed */}
       <Modal
-  title="User Details"
-  visible={showWalletModal}
-  onCancel={handleModalClose}
-  footer={[
-    <Button key="close" onClick={handleModalClose}>
-      Close
-    </Button>,
-    <Button key="addMoney" type="primary" onClick={() => handleAddMoney(userData?.wallet_id, amountToAdd, otp)}>
-      Add Money
-    </Button>,
-  ]}
-  centered
-  maskClosable={false}
-  closable={false}
-  style={{ maxWidth: '400px' }} // Adjust the maximum width as needed
->
-  {userData && (
-    <>
-      <p>
-        <strong>User ID:</strong> {userData.id}
-      </p>
-      <p>
-        <strong>Username:</strong> {userData.name}
-      </p>
-      <Input
-        placeholder="Amount to Add"
-        onChange={(e) => setAmountToAdd(e.target.value)}
-        value={amountToAdd}
-        style={{ marginBottom: '10px' }} // Add some bottom margin to separate input fields
-      />
-      <Input
-        placeholder="Employee Security Pin"
-        onChange={(e) => setOtp(e.target.value)}
-        value={otp}
-      />
-    </>
-  )}
-</Modal>
+        title="User Details"
+        visible={showWalletModal}
+        onCancel={handleModalClose}
+        footer={[
+          <Button key="close" onClick={handleModalClose}>
+            Close
+          </Button>,
+          <Button key="addMoney" type="primary" onClick={() => handleAddMoney(userData?.wallet_id, amountToAdd, otp)}>
+            Add Money
+          </Button>,
+        ]}
+        centered
+        maskClosable={false}
+        closable={false}
+        style={{ maxWidth: '400px' }} // Adjust the maximum width as needed
+      >
+        {userData && (
+          <>
+            <p>
+              <strong>User ID:</strong> {userData.id}
+            </p>
+            <p>
+              <strong>Username:</strong> {userData.name}
+            </p>
+            <Input
+              placeholder="Amount to Add"
+              onChange={(e) => setAmountToAdd(e.target.value)}
+              value={amountToAdd}
+              style={{ marginBottom: '10px' }} // Add some bottom margin to separate input fields
+            />
+            <Input
+              placeholder="Employee Security Pin"
+              onChange={(e) => setOtp(e.target.value)}
+              value={otp}
+            />
+          </>
+        )}
+      </Modal>
     </>
   );
 };
